294fb97f4d4ef76cef1725afd0d400ef1c3258a9
AmraCF <amrabeganovic@Amras-MacBook-Air.local>
2014-10-09 19:11:33 -0400

    clubfashionista

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_action :correct_user,   only: [:index, :edit, :update, :destroy]
+
 
 	def show
     @user = User.find(params[:id])
@@ -8,20 +11,10 @@ class UsersController < ApplicationController
     @user = User.new
 	end
 
-  def edit
-    @user = User.find(params[:id])
+  def index
+  @users = User.all
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
-  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -31,12 +24,35 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end 
+
+  def edit
   end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
