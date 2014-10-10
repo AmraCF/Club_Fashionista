a333b41201ea718c64eb894b5f57c3ff83b7ac78
AmraCF <amrabeganovic@Amras-MacBook-Air.local>
2014-10-09 21:33:51 -0400

    clubfashionista

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name     "Amra Beganovic"
-    email    "eurolade@aol.com"
-    password "rajvosa"
-    password_confirmation "rajvosa"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
